d1bee18b41d21b4483f20898502f4c7337c3abd3
lisadimitrief <84084978+lisadimitrief@users.noreply.github.com>
2023-07-05 09:17:38 +0200

    Revert "1er commit"
    This reverts commit f58b62ba25cff80daeac4eb4c9953b7df2b7e560.

--- API-ProjetFilRouge/src/main/java/com/cda/API/ProjetFilRouge/repository/Equipement_dragonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cda.API.ProjetFilRouge.repository;
-
-import com.cda.API.ProjetFilRouge.modele.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Equipement_dragonRepository extends JpaRepository<Equipement_dragon, Long> {
-
-}
