5e3c0e033c3d0bed8ce6dc8fabbb4e47c8aecb28
Kazetus <105437345+Kazetus@users.noreply.github.com>
2023-07-07 16:12:37 +0200

    Functionning Register, not connexion

--- api/src/main/java/com/draconomicon/api/config/SecurityConfig.java
+++ api/src/main/java/com/draconomicon/api/config/SecurityConfig.java
@@ -2,14 +2,12 @@ package com.draconomicon.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,7 +17,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig{
-   
+    private final JwtAuthenticationFilter jwtAuthFilter;
+	private final AuthenticationProvider authenticationProvider;
 	@Bean
 	public SecurityFilterChain apiSecurity(HttpSecurity http) throws Exception {
         http
@@ -27,31 +26,16 @@ public class SecurityConfig{
         .csrf((csrf) -> csrf.disable())
         .authorizeHttpRequests((auth) -> auth
         				.requestMatchers("/login").permitAll()
+        				.requestMatchers("/register").permitAll()
         				.requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest()
                         .authenticated()
-                        .and()
         )
-                .httpBasic(withDefaults());
-		
+        .sessionManagement((auth) -> auth
+        			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        		)
+        .authenticationProvider(authenticationProvider)
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
-	}
-	@Bean
-	public InMemoryUserDetailsManager userDetailsService() {
-		UserDetails user = User.builder()
-				.username("user")
-				.password(passwordEncoder().encode("user"))
-				.roles("USER")
-				.build();
-		UserDetails admin = User.builder()
-				.username("admin")
-				.password(passwordEncoder().encode("user"))
-				.roles("ADMIN")
-				.build();
-		return new InMemoryUserDetailsManager(user, admin);
-	}
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 }
